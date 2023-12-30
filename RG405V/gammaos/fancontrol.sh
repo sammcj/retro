ffb7a928c633448d6b904419b9cd15aab62edf19
Sam McLeod <sammcj@users.noreply.github.com>
2023-12-30 12:52:59 +1100

    minor updates

--- RG405V/gammaos/fancontrol.sh
+++ RG405V/gammaos/fancontrol.sh
@@ -35,43 +35,42 @@ done
 
 # Now we run a background loop to check the temperature and adjust the fan speed, if it crashes it will be restarted
 while true; do
-  # If Android is sleeping, set the fan speed to 0 and wait for it to wake up
-  STATE=$(dumpsys power | grep mWakefulness= | awk '{print $1}')
-  if [ "$STATE" != "mWakefulness=Awake" ] && [ "$STATE" != "mWakefulness=Dozing" ]; then
-    echo 0 >$FAN_SPEED
-    sleep 30
-    continue
-  fi
-
   # Get CPU temperature
   temp=$(echo "$(cat ${CPU_TEMP}) / 1000" | bc)
 
-  # Set the pwm speed for the fan based on the temperature (0 at below 50C, 90 at 70C, 140 at 75C, 250 at or above 85C)
-  if [ "$temp" -lt 50 ]; then
+  # If Android is sleeping, set the fan speed to 0 and wait for it to wake up
+  STATE=$(dumpsys power | grep mWakefulness= | awk '{print $1}')
+  if [ "$STATE" != "mWakefulness=Awake" ]; then
     enable=0
     speed=0
-  elif [ "$temp" -lt 60 ]; then
-    enable=1
-    speed=50
-  elif [ "$temp" -lt 65 ]; then
-    enable=1
-    speed=90
-  elif [ "$temp" -lt 70 ]; then
-    enable=1
-    speed=150
-  elif [ "$temp" -lt 75 ]; then
-    enable=1
-    speed=200
-  elif [ "$temp" -ge 80 ]; then
-    enable=1
-    speed=250
   else
-    enable=0
-    speed=0
-  fi
+    # Set the pwm speed for the fan based on the temperature range
+    if [ "$temp" -lt 50 ]; then
+      enable=0
+      speed=0
+    elif [ "$temp" -lt 60 ]; then
+      enable=1
+      speed=50
+    elif [ "$temp" -lt 65 ]; then
+      enable=1
+      speed=90
+    elif [ "$temp" -lt 70 ]; then
+      enable=1
+      speed=150
+    elif [ "$temp" -lt 75 ]; then
+      enable=1
+      speed=200
+    elif [ "$temp" -ge 80 ]; then
+      enable=1
+      speed=250
+    else
+      enable=0
+      speed=0
+    fi
 
-  # Multiply the speed by the optional multiplier
-  speed=$((speed * SPEED_MULTIPLIER))
+    # Multiply the speed by the optional multiplier
+    speed=$((speed * SPEED_MULTIPLIER))
+  fi
 
   if $DEBUG; then
     echo "CPU temp: ${temp} C, Fan PWM: ${speed}, Fan enabled: ${enable}"
